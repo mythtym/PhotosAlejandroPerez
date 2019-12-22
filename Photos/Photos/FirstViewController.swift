4a6d2988d80e058b5bdd14c70db523b70a7ca823
alejandro <scanf16@hotmail.com>
2019-12-22 12:28:17 -0500

    First commit

---
+++ Photos/Photos/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Photos
+//
+//  Created by alejandro david perez morales on 12/22/19.
+//  Copyright © 2019 alejandro david perez morales. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Photos/Photos/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Photos
+//
+//  Created by alejandro david perez morales on 12/22/19.
+//  Copyright © 2019 alejandro david perez morales. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
